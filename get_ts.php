443d06481093d89363fe5e25ef5e4116e14f6645
FTesla <bfd007@mail.ru>
2016-04-04 05:56:36 +0600

    Загрузка конечной версии

---
+++ get_ts.php
@@ -0,0 +1,13 @@
+﻿<?php 
+	$l = $_GET["l"];
+	
+	$start = microtime(true);
+	$ts = file_get_contents($l);
+	$time = microtime(true) - $start;
+	
+	$size = strlen($ts);
+	
+	$v = round($size/$time*8);
+	
+	echo $size.':'.$v;
+?>
---
+++ playlist.php
@@ -0,0 +1,6 @@
+﻿<?php 
+	$l = $_GET["l"];
+	$html = file_get_contents($l);
+	
+	echo $html;
+?>
